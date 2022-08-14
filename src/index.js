4c09682e201309055398ae73aa5ad942a789ded8
audreyper <audreyper@protonmail.com>
2022-08-14 15:25:16 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import store from './app/store.js';
+import App from './app/App.js';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
@@ -21,3 +21,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
